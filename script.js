a79428d6ad858eb83e9627214d8314e7cd388436
josé Caldas <jrcaldas@yahoo.com.br>
2020-09-20 12:15:06 -0300

    menu responsivo com JQuery

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(".bot").click(function () {
+//   alert("clicou");
+// });
+
+$(document).ready(function () {
+  $(".open-nav").click(function () {
+    $(".menu").toggleClass("active");
+    $(".open-nav").toggleClass("open");
+  });
+});
